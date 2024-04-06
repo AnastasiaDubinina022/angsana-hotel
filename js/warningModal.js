efd0f5a19716194d2055e731158125899b857116
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-04-06 14:48:57 +0300

    Added warningModal.js

--- js/warningModal.js
+++ js/warningModal.js
@@ -1,5 +1,51 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
+    function createModal() {
+        const body =  document.querySelector('body');
+        const wrapper = document.querySelector('.wrapper');
+        const modalWrapper = document.createElement('div');
+        const modalText = document.createElement('div');
 
+        modalWrapper.classList.add('warning-modal');
+        modalText.classList.add('warning-modal__text');
+
+        modalText.textContent = 'This section is currently under development.';
+        modalWrapper.append(modalText);
+        wrapper.append(modalWrapper);
+
+        const showModal = () => {
+            modalWrapper.classList.remove('warning-modal_hide');
+            modalWrapper.classList.add('warning-modal_show');
+            body.classList.remove('unlocked-scroll');
+            body.classList.add('locked-scroll');
+
+        };
+
+        const hideModal = () => {
+            modalWrapper.classList.remove('warning-modal_show');
+            modalWrapper.classList.add('warning-modal_hide');
+            body.classList.remove('locked-scroll');
+            body.classList.add('unlocked-scroll');
+        };
+
+        modalWrapper.addEventListener('click', (event) => {
+            if (event.target.classList.value !== 'warning-modal__text') {
+                hideModal();
+            }
+        });
+        
+        const blueButtonsList = document.querySelectorAll('.blue-button');
+        const yellowButtonsList = document.querySelectorAll('.yellow-button');
+        const totalButtonsList = [...blueButtonsList, ...yellowButtonsList];
+
+        totalButtonsList.forEach((item) => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+                showModal();
+            });
+        });
+    }
+
+    createModal();
 });
