3b39f0eb75b64ed1bfc07090363bb66e3cc6f000
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-08-30 15:19:30 +0300

    Slider added into home page.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-import createWarningModal from "./modules/warningModal.js";
-import homeAboutTabs from "./modules/homeAboutTabs.js";
-import createInfoCards from "./modules/infoCards.js";
+import createWarningModal from "./modules/warning-modal.js";
+import homeAboutTabs from "./modules/home-about-tabs.js";
+import createInfoCards from "./modules/info-cards.js";
+import homeGallerySlider from "./modules/home-galery-slider.js";
 
 
 window.addEventListener('DOMContentLoaded', () => {
+    homeGallerySlider();  
     homeAboutTabs('.tabheader__item', '.tabcontent', '.tabheader', 'tabheader__item_active');
     createInfoCards();
     createWarningModal();
+          
 });
